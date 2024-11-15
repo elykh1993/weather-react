e6fb6f5763cf2b633283b088863060912084ad04
lea-bee <twigg.lt20@gmail.com>
2024-11-15 16:01:56 +0200

    Updated CSS of project

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -22,7 +22,7 @@ export default function WeatherInfo(props) {
         </div>
         <div className="col-lg-6 col-md-6 col-sm-8">
           <div className="temperature-container d-flex justify-content-end">
-            <WeatherIcon code={props.data.icon} size={52} />
+            <WeatherIcon code={props.data.icon} size={58} />
             <div className="float-left">
               <WeatherTemperature celsius={props.data.temperature} />
             </div>
