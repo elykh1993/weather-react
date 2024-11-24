895a43c420c185b26966f0973043edeb6a93f981
elykh1993 <120404324+elykh1993@users.noreply.github.com>
2024-11-24 14:53:32 +0330

    App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <div className="container">
-        <Weather defaultCity="San Francisco" />
+        <Weather defaultCity="Tehran" />
       </div>
     </div>
   );
